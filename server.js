0b54a8f075e1958f0a703de65b6fbdf85928e7cb
masnad <masnad.nehith@gmail.com>
2018-07-01 19:22:16 +0300

    Posting With Postman

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.post('/',(req,res)=>{
+/* handling all the parsing */
+app.use(bodyParser.json());
+
+app.post('/', (req,res)=>{
   var email = req.body.email;
   var amount = req.body.amount;
-
-  res.send({"amount" : amount , "email" : email });
+  res.send({'amount' : amount, 'email': email});
 });
 
 app.listen(3000,()=>{

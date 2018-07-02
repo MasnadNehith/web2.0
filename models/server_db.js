626fd26adaf82d1d28ec5c814ed687b14232e53e
masnad <masnad.nehith@gmail.com>
2018-07-02 21:07:45 +0300

    Getting Data Back

--- models/server_db.js
+++ models/server_db.js
@@ -9,6 +9,16 @@ save_user_information = (data) => new Promise((resolve,reject)=>{
   });
 })
 
+
+get_total_amount = (data) => new Promise((resolve,reject)=>{
+  db.query('select sum(amount) as total_amount from lottery_information ',null, function(err,results, fields){
+    if(err){
+      reject('Could not get total amount');
+    }
+    resolve(results);
+  });
+})
+
 module.exports = {
   save_user_information
 }
--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.post('/', async (req,res)=>{
   res.send(result);
 });
 
+app.get('/get_total_amount', async (req,res)=>{
+  var result = await get_total_amount();
+  res.send(result);
+});
+
 app.listen(3000,()=>{
   console.log('server is running on port 3000');
 });

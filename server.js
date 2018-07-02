b4ff306f813a60d02fe62fd42adf55ba541375d8
masnad <masnad.nehith@gmail.com>
2018-07-02 20:20:12 +0300

    Validation Message

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 app.post('/', (req,res)=>{
   var email = req.body.email;
   var amount = req.body.amount;
+
+  if(amount <= 1){
+    return_info = {};
+    return_info.error = true;
+    return_info.message = "The amount should be greater than 1";
+    return res.send(return_info);
+  }
+
   res.send({'amount' : amount, 'email': email});
 });
 

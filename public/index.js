9e0de5ba4e909e79b87660a75def852674d60e79
masnad <masnad.nehith@gmail.com>
2018-07-07 19:33:52 +0300

    Post With AXIOS

--- public/index.js
+++ public/index.js
@@ -2,8 +2,18 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      'total_amount' : 1000
+      'total_amount' : 1000,
+      'amount' : 100,
+      'email' : '',
     }
+  }
+  onSubmit = async (event) =>{
+    event.preventDefault();
+    const response = await axios.post('/post_info',{
+      amount : this.state.amount,
+      email : this.state.email
+    })
+    console.log(response);
   }
   render(){
     return(
@@ -12,10 +22,12 @@ class App extends React.Component{
         <div>
           <p> Total lottery amount is {this.state.total_amount}</p>
         </div>
-        <form>
-          <input placeholder="amount" />
-          <input placeholder="email" />
-          <button> Participate </button>
+        <form onSubmit={this.onSubmit}>
+          <input placeholder="amount" value = {this.state.amount}
+            onChange = {event=> this.setState({amount : event.target.value})}/>
+          <input placeholder="email" value = {this.state.email}
+            onChange = {event=> this.setState({email : event.target.value})} />
+          <button type="submit" > Participate </button>
         </form>
       </div>
     )
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const publicPath = path.join(__dirname, './public');
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
-app.post('/', async (req,res)=>{
+app.post('/post_info', async (req,res)=>{
   var email = req.body.email;
   var amount = req.body.amount;
 

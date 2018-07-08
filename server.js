aa33820c490e696537aae9f5bbe8a17816823c79
masnad <masnad.nehith@gmail.com>
2018-07-08 20:03:04 +0300

    Paying The Winner

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ app.get('/pick_winner', async (req,res)=>{
           console.log(payment);
           for(var i = 0; i< payment.links.length; i++){
             if(payment.links[i].rel =='approval_url'){
-              return res.send(payment.links[i].href);
+              return res.redirect(payment.links[i].href);
             }
           }
       }
